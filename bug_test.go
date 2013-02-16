b43bfbede7fb7ab448efd6f49030629ac713ec77
awalterschulze@gmail.com <awalterschulze@gmail.com>
2013-02-16 16:29:17 +0200

    fixed test

--- bug_test.go
+++ bug_test.go
@@ -7,21 +7,23 @@ import (
 )
 
 type bugSubGraphWorldVisitor struct {
-	g *Graph
-	t *testing.T
+	t     *testing.T
+	found bool
 }
 
 func (this *bugSubGraphWorldVisitor) Visit(v ast.Elem) ast.Visitor {
-	edge, ok := v.(*ast.EdgeStmt)
+	edge, ok := v.(ast.EdgeStmt)
 	if !ok {
 		return this
 	}
-	if edge.Source.GetId() != "2" {
+	if edge.Source.GetId().String() != "2" {
 		return this
 	}
 	dst := edge.EdgeRHS[0].Destination
 	if _, ok := dst.(*ast.SubGraph); !ok {
 		this.t.Fatalf("2 -> Not SubGraph")
+	} else {
+		this.found = true
 	}
 	return this
 }
@@ -31,8 +33,10 @@ func TestBugSubGraphWorld(t *testing.T) {
 	st, err := parser.ParseString(g.String())
 	check(t, err)
 	s := &bugSubGraphWorldVisitor{
-		g: g,
 		t: t,
 	}
 	st.Walk(s)
+	if !s.found {
+		t.Fatalf("2 -> SubGraph not found")
+	}
 }
