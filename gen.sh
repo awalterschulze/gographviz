1cb826bfa548bc714ba13772cdbe9df2bff4870f
Walter Schulze <awalterschulze@gmail.com>
2016-10-01 09:29:01 +0200

    updated travis to regenerate and check diff

--- gen.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-gocc dot.bnf
-find . -type f -name '*.go' | xargs goimports -w
---
+++ install-godeps.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+set -xe
+mkdir -p $GOPATH/src/githbub.com/goccmack
+git clone https://github.com/goccmack/gocc $GOPATH/src/github.com/goccmack/gocc
